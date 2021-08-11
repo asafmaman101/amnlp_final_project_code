56009f7bb4869065b7c6ad4141e9b13a4c821e4f
asafmaman101 <asafmaman@icloud.com>
2021-08-11 21:31:43 +0300

    Export run experiment function from cli.py main (to facilitate external use).

--- cli.py
+++ cli.py
@@ -18,6 +18,7 @@ one of the supported tasks and datasets.
 import argparse
 import json
 import os
+from datetime import datetime
 from typing import Tuple
 import pandas as pd
 
@@ -89,6 +90,93 @@ def load_ipet_config(args) -> pet.IPetConfig:
     ipet_cfg = pet.IPetConfig(generations=args.ipet_generations, logits_percentage=args.ipet_logits_percentage,
                               scale_factor=args.ipet_scale_factor, n_most_likely=args.ipet_n_most_likely)
     return ipet_cfg
+
+
+def run_experiment_given_arguments(args):
+
+    timer = Timer('end-to-end')
+
+    args.output_dir = args.output_dir.format(**args.__dict__)
+    # args.output_dir += f'_{datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")}'
+
+    if os.path.exists(args.output_dir) and os.listdir(args.output_dir) \
+            and args.do_train and not args.overwrite_output_dir:
+        raise ValueError("Output directory ({}) already exists and is not empty.".format(args.output_dir))
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    with open(os.path.join(args.output_dir, 'config.json'), 'w') as f:
+        json.dump(args.__dict__, f, indent=4)
+
+    file_handler = logging.FileHandler(os.path.join(args.output_dir, 'console.log'))
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    logger.info("Parameters:\n{}".format(pd.Series(args.__dict__)))
+
+    # Setup CUDA, GPU & distributed training
+    gpus_string_list = map(lambda x: str(x), args.visible_gpus)
+    os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(gpus_string_list)
+    args.device = "cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu"
+    args.n_gpu = torch.cuda.device_count()
+
+    # Prepare task
+    args.task_name = args.task_name.lower()
+
+    # TODO: change to support multi-task processing
+    if args.task_name not in PROCESSORS:
+        raise ValueError("Task '{}' not found".format(args.task_name))
+    processor = PROCESSORS[args.task_name]()
+
+    args.label_list = processor.get_labels()
+
+    train_ex_per_label, test_ex_per_label = None, None
+    train_ex, test_ex = args.train_examples, args.test_examples
+
+    if args.split_examples_evenly:
+        train_ex_per_label = eq_div(args.train_examples, len(args.label_list)) if args.train_examples != -1 else -1
+        test_ex_per_label = eq_div(args.test_examples, len(args.label_list)) if args.test_examples != -1 else -1
+        train_ex, test_ex = None, None
+
+    eval_set = TEST_SET if args.eval_set == 'test' else DEV_SET
+
+    train_data = load_examples(
+        args.task_name, args.data_dir, TRAIN_SET, num_examples=train_ex, num_examples_per_label=train_ex_per_label)
+    eval_data = load_examples(
+        args.task_name, args.data_dir, eval_set, num_examples=test_ex, num_examples_per_label=test_ex_per_label)
+    unlabeled_data = load_examples(
+        args.task_name, args.data_dir, UNLABELED_SET, num_examples=args.unlabeled_examples)
+
+    args.metrics = METRICS.get(args.task_name, DEFAULT_METRICS)
+
+    pet_model_cfg, pet_train_cfg, pet_eval_cfg = load_pet_configs(args)
+    sc_model_cfg, sc_train_cfg, sc_eval_cfg = load_sequence_classifier_configs(args)
+    ipet_cfg = load_ipet_config(args)
+
+    if args.method == 'pet':
+        pet.train_pet(pet_model_cfg, pet_train_cfg, pet_eval_cfg, sc_model_cfg, sc_train_cfg, sc_eval_cfg,
+                      pattern_ids=args.pattern_ids, output_dir=args.output_dir,
+                      ensemble_repetitions=args.pet_repetitions, final_repetitions=args.sc_repetitions,
+                      reduction=args.reduction, train_data=train_data, unlabeled_data=unlabeled_data,
+                      eval_data=eval_data, do_train=args.do_train, do_eval=args.do_eval,
+                      no_distillation=args.no_distillation, seed=args.seed)
+
+    elif args.method == 'ipet':
+        pet.train_ipet(pet_model_cfg, pet_train_cfg, pet_eval_cfg, ipet_cfg, sc_model_cfg, sc_train_cfg, sc_eval_cfg,
+                       pattern_ids=args.pattern_ids, output_dir=args.output_dir,
+                       ensemble_repetitions=args.pet_repetitions, final_repetitions=args.sc_repetitions,
+                       reduction=args.reduction, train_data=train_data, unlabeled_data=unlabeled_data,
+                       eval_data=eval_data, do_train=args.do_train, do_eval=args.do_eval, seed=args.seed)
+
+    elif args.method == 'sequence_classifier':
+        pet.train_classifier(sc_model_cfg, sc_train_cfg, sc_eval_cfg, output_dir=args.output_dir,
+                             repetitions=args.sc_repetitions, train_data=train_data, unlabeled_data=unlabeled_data,
+                             eval_data=eval_data, do_train=args.do_train, do_eval=args.do_eval, seed=args.seed)
+
+    else:
+        raise ValueError(f"Training method '{args.method}' not implemented")
+    logger.info(timer.elapsed_str())
 
 
 def main():
@@ -216,85 +304,13 @@ def main():
                         help="Whether to use priming for evaluation")
     parser.add_argument("--eval_set", choices=['dev', 'test'], default='dev',
                         help="Whether to perform evaluation on the dev set or the test set")
-
-    timer = Timer('end-to-end')
+    parser.add_argument("--visible_gpus", required=False, default=[0], type=int, nargs="+",
+                        help="Specify which gpus to use. ")
 
     args = parser.parse_args()
 
-    if os.path.exists(args.output_dir) and os.listdir(args.output_dir) \
-            and args.do_train and not args.overwrite_output_dir:
-        raise ValueError("Output directory ({}) already exists and is not empty.".format(args.output_dir))
+    run_experiment_given_arguments(args)
 
-    os.makedirs(args.output_dir, exist_ok=True)
-
-    with open(os.path.join(args.output_dir, 'config.json'), 'w') as f:
-        json.dump(args.__dict__, f, indent=4)
-
-    file_handler = logging.FileHandler(os.path.join(args.output_dir, 'console.log'))
-    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-    # logger.info("Parameters:\n{}".format(json.dumps(args.__dict__, indent=2)))
-    logger.info("Parameters:\n{}".format(pd.Series(args.__dict__)))
-
-    # Setup CUDA, GPU & distributed training
-    args.device = "cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu"
-    args.n_gpu = torch.cuda.device_count()
-
-    # Prepare task
-    args.task_name = args.task_name.lower()
-    if args.task_name not in PROCESSORS:
-        raise ValueError("Task '{}' not found".format(args.task_name))
-    processor = PROCESSORS[args.task_name]()
-    args.label_list = processor.get_labels()
-
-    train_ex_per_label, test_ex_per_label = None, None
-    train_ex, test_ex = args.train_examples, args.test_examples
-    if args.split_examples_evenly:
-        train_ex_per_label = eq_div(args.train_examples, len(args.label_list)) if args.train_examples != -1 else -1
-        test_ex_per_label = eq_div(args.test_examples, len(args.label_list)) if args.test_examples != -1 else -1
-        train_ex, test_ex = None, None
-
-    eval_set = TEST_SET if args.eval_set == 'test' else DEV_SET
-
-    train_data = load_examples(
-        args.task_name, args.data_dir, TRAIN_SET, num_examples=train_ex, num_examples_per_label=train_ex_per_label)
-    eval_data = load_examples(
-        args.task_name, args.data_dir, eval_set, num_examples=test_ex, num_examples_per_label=test_ex_per_label)
-    unlabeled_data = load_examples(
-        args.task_name, args.data_dir, UNLABELED_SET, num_examples=args.unlabeled_examples)
-
-    args.metrics = METRICS.get(args.task_name, DEFAULT_METRICS)
-
-    pet_model_cfg, pet_train_cfg, pet_eval_cfg = load_pet_configs(args)
-    sc_model_cfg, sc_train_cfg, sc_eval_cfg = load_sequence_classifier_configs(args)
-    ipet_cfg = load_ipet_config(args)
-
-    if args.method == 'pet':
-        pet.train_pet(pet_model_cfg, pet_train_cfg, pet_eval_cfg, sc_model_cfg, sc_train_cfg, sc_eval_cfg,
-                      pattern_ids=args.pattern_ids, output_dir=args.output_dir,
-                      ensemble_repetitions=args.pet_repetitions, final_repetitions=args.sc_repetitions,
-                      reduction=args.reduction, train_data=train_data, unlabeled_data=unlabeled_data,
-                      eval_data=eval_data, do_train=args.do_train, do_eval=args.do_eval,
-                      no_distillation=args.no_distillation, seed=args.seed)
-
-    elif args.method == 'ipet':
-        pet.train_ipet(pet_model_cfg, pet_train_cfg, pet_eval_cfg, ipet_cfg, sc_model_cfg, sc_train_cfg, sc_eval_cfg,
-                       pattern_ids=args.pattern_ids, output_dir=args.output_dir,
-                       ensemble_repetitions=args.pet_repetitions, final_repetitions=args.sc_repetitions,
-                       reduction=args.reduction, train_data=train_data, unlabeled_data=unlabeled_data,
-                       eval_data=eval_data, do_train=args.do_train, do_eval=args.do_eval, seed=args.seed)
-
-    elif args.method == 'sequence_classifier':
-        pet.train_classifier(sc_model_cfg, sc_train_cfg, sc_eval_cfg, output_dir=args.output_dir,
-                             repetitions=args.sc_repetitions, train_data=train_data, unlabeled_data=unlabeled_data,
-                             eval_data=eval_data, do_train=args.do_train, do_eval=args.do_eval, seed=args.seed)
-
-    else:
-        raise ValueError(f"Training method '{args.method}' not implemented")
-
-    logger.info(timer.elapsed_str())
 
 if __name__ == "__main__":
     main()
